6bfc7449635e7f62bd3bdc23db2f06c5417efb9f
TestUser <s.c1@mail.com>
2025-02-09 12:23:30 +0530

    deploy

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,9 +1,8 @@
 provider "azurerm" {
   features {}
 
-  # Using the environment variables set in GitHub Actions
-  client_id       = getenv("ARM_CLIENT_ID")
-  tenant_id       = getenv("ARM_TENANT_ID")
-  client_secret   = getenv("ARM_CLIENT_SECRET")
-  subscription_id = getenv("ARM_SUBSCRIPTION_ID")
+  client_id       = var.client-id
+  tenant_id       = var.tenant-id
+  client_secret   = var.client-secret
+  subscription_id = var.subscription-id
 }
